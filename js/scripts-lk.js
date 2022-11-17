111f16e8a95e2c7cdb972fdb256ebe551cb31ca7
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-11-17 12:31:51 +0300

    Fix 5

--- js/scripts-lk.js
+++ js/scripts-lk.js
@@ -41,6 +41,93 @@ $(() => {
       // Кастомный select
 	    $('select').niceSelect()
 
+
+
+      // Моб. меню в личный кабинет
+      $('.lk header .mob_menu_btn').click((e) => {
+        e.preventDefault()
+
+        $('.lk header .mob_menu_btn').addClass('active')
+        $('body').addClass('menu_open')
+        $('.main-content_aside').addClass('show')
+        $('.overlay').fadeIn(300)
+    })
+
+    $('.lk header .close_btn, .overlay').click((e) => { 
+
+        $('.lk header .mob_menu_btn').removeClass('active')
+        $('body').removeClass('menu_open')
+        $('.main-content_aside').removeClass('show')
+        $('.overlay').fadeOut(300)
+    })
+
+
+
+      // Изменение количества товара
+      $('body').on('click', '.steps_info-amount .minus', function(e) {
+        e.preventDefault()
+
+        let parent = $(this).closest('.amount')
+        let input = parent.find('.input')
+        let inputVal = parseFloat( input.val() )
+        let minimum = parseFloat( input.data('minimum') )
+        let step = parseFloat( input.data('step') )
+        let unit = input.data('unit')
+
+        if( inputVal > minimum ){
+          input.val( inputVal-step+unit )
+        }
+
+        if( $(this).hasClass('update_price') ){
+          updateCartPrice( $(this).closest('.product') )
+        }
+    })
+
+    $('body').on('click', '.steps_info-amount .plus', function(e) {
+        e.preventDefault()
+
+        let parent = $(this).closest('.amount')
+        let input = parent.find('.input')
+        let inputVal = parseFloat( input.val() )
+        let maximum = parseFloat( input.data('maximum') )
+        let step = parseFloat( input.data('step') )
+        let unit = input.data('unit')
+
+        if( inputVal < maximum ){
+          input.val( inputVal+step+unit )
+        }
+
+        if( $(this).hasClass('update_price') ){
+          updateCartPrice( $(this).closest('.product') )
+        }
+    })
+
+    $('.steps_info-amount .input').keydown(function() {
+      let _self = $(this)
+
+      setTimeout(function(){
+        if(_self.val() == '' || _self.val() == 0){
+          _self.val( parseInt(_self.data('minimum')) )
+        }
+
+          if( _self.hasClass('update_price') ){
+            updateCartPrice( _self.closest('.product') )
+          }
+      }, 10)
+    })
+
+
+    function updateCartPrice(context){
+      if( context ){
+        let price = parseInt(context.find('.price:not(.total)').data('price'))
+        let amount = parseInt(context.find('.amount .input').val())
+        let totalPrice = 15*amount
+    
+        context.find('.steps_info-calculator .price.total span').text( totalPrice )
+      }
+    
+    }
+
     
        
     });
