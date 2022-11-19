407c63dec8703aa11c8c1c64bba9dda54b8653c7
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-11-19 20:47:05 +0300

    222

--- js/scripts-lk.js
+++ js/scripts-lk.js
@@ -133,7 +133,28 @@ $(() => {
       
 
 
-      
+      new Sortable(example1, {
+        animation: 150,
+        ghostClass: 'blue-background-class'
+      });
+
+
+      // Аккордион
+      $('body').on('click', '.accordion .accordion_item .head', function (e) {
+        e.preventDefault()
+
+        const $item = $(this).closest('.accordion_item'),
+          $accordion = $(this).closest('.accordion')
+
+        if ($item.hasClass('active')) {
+          $item.removeClass('active').find('.data').slideUp(300)
+        } else {
+          $accordion.find('.accordion_item').removeClass('active')
+          $accordion.find('.data').slideUp(300)
+
+          $item.addClass('active').find('.data').slideDown(300)
+        }
+      })
 
 
 
